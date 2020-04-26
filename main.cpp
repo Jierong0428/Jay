30451536cd88a5171330f95531ad8f2d475f0530
Jierong0428 <54598244+Jierong0428@users.noreply.github.com>
2020-04-26 06:08:00 -0400

    power method

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <math.h>       /* pow */
-#include <stdio.h>
+//
+//  main.cpp
+//  power method
+//
+//  Created by 罗杰榕 on 11/7/19.
+//  Copyright © 2019 Jierong. All rights reserved.
+//
+
+
+#include<iostream>
+#include<vector>
 #include<cmath>
-#include<ctime>
-#include<cstdlib>
 using namespace std;
 
-
-// a bunch of useful functions first
-// implement matrix multiplication in a function
-vector<double> array_multiply_matrix(vector<double> array, vector< vector<double> > matrix) {
+//Implement matrix multiplication in a function
+vector<double> matrix_multiply_array(vector<vector<double>> matrix, vector<double> array)
+{
     //Get the row of matrix
     const unsigned short row = matrix.size();
     //Get the column of matrix
@@ -18,469 +22,149 @@ vector<double> array_multiply_matrix(vector<double> array, vector< vector<double
     //Get the length of array
     const unsigned short len_of_vector = array.size();
 
+
     vector<double>  res;
-    if(row != len_of_vector) {  // if it can't be multiplied, return None
-        cout << "error input"<<endl;
+    if (col != len_of_vector)//if it can't be multiplied, return None
+    {
         return res;
     }
-    else {
+    else
+    {
         //initialize the result array
-        res.resize(col);
+        res.resize(row);
+
+
         //multiplication
-        for(int j = 0; j < col; j++) {
-            for(int i = 0; i < row; i++) {
-                res[j] += matrix[i][j] * array[i];
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+
+                res[i] += matrix[i][j] * array[j];
             }
         }
     }
+
     return res;
 }
 
 
-double GaussRand(double dExpect, double dVariance) {
-    static double V1, V2, S;
-    static int phase = 0;
-    double X;
-    srand(time(0));
-    if(phase == 0) {
-        do {
-            double U1 = (double)rand() / RAND_MAX;
-            double U2 = (double)rand() / RAND_MAX;
-
-            V1 = 2 * U1 - 1;
-            V2 = 2 * U2 - 1;
-            S = V1 * V1 + V2 * V2;
-        } while(S >= 1 || S == 0);
-        X = V1 * sqrt(-2 * log(S) / S);
-    } else
-        X = V2 * sqrt(-2 * log(S) / S);
-    phase = 1 - phase;
-    return X * dVariance + dExpect;
+//calcuate the norm of a vector
+double getnorm(vector<double> array){
+    double res = 0;
+    const unsigned short len = array.size();
+    for (unsigned i = 0;i < len;i++){
+        res += pow(array.at(i),2);
+    }
+    res = sqrt(res);
+    return res;
 }
-// derivative of activate function
-double f_prime(double num) {
-    if (num > 0) {
-        return 1;
-    }
-    else if(num == 0) {
-        return 0.5;
-    }
-    else {
-        return 0;
-    }
-}
-
-
-// activation function
-double ReLU(double input) {
-    if(input > 0) {
-        return input;
-    }
-    else {
-        return 0;
-    }
-}
-
-
-// activate vector aka use ReLU
-vector<double> activate(vector<double> input){
-    vector<double> output;
-    for(int i = 0; i < input.size(); i++) {
-        output.push_back(ReLU(input[i]));
-    }
-    return output;
-}
-
-
-// initialize output for each neuron each day
-vector< vector<double> > X;
-vector< vector<double> > Y;
-vector< vector<double> > Z;
-vector< double> F;  // final layer output for each day
-
-
-// initialize the matrices to hold output vectors for all dates
-void calc_output(vector< vector<double> > weights_1, vector< vector<double> > weights_2,
-                 vector<double> weights_3, vector< vector<double> > df) {
-
-
-    for (int i = 0; i < df.size(); i++) {
-        F[i] = 0.0;
-    }
-    for (int i = 0; i < X.size(); i++) {
-        // x's, y's and z's hold the output of each layer
-        X[i] = activate(df[i]);
-        Y[i] = activate(array_multiply_matrix(X[i], weights_1));
-        Z[i] = activate(array_multiply_matrix(Y[i], weights_2));
-        for (int j = 0; j < 5; j++) {
-            F[i] += Z[i][j] * weights_3[j];
-
-        }
-
-        F = activate(F);
-
-    }
-}
-
-
-// loss function
-double get_loss(const vector<double> volume ) {
-    double ret = 0;
-    int T = volume.size();
-    for(int i = 0; i < T; i++) {
-        ret += pow((F[i] - volume[i]), 2);
-        //cout << " Output :" << F[i] << endl;
-    }
-    ret /= T;
-    return ret;
-}
-
-
-vector<double> normalize(vector<double> vec) {
-    double s_mean = 0;
-    double s_var = 0;
-    for(int i = 0; i < vec.size(); i++) {
-        s_mean += vec[i];
-    }
-    s_mean /= vec.size();
-    for(int i = 0; i < vec.size(); i++) {
-        s_var += (s_mean - vec[i]) * (s_mean - vec[i]);
-    }
-    s_var /= vec.size();
-    for(int i = 0; i < vec.size(); i++) {
-        vec[i] = (vec[i] - s_mean) / pow(s_var, 0.5);
-    }
-    return vec;
-}
-
-
-vector<double> get_column(vector< vector<double> > df, int col) {
+//mutiply a vector by a number
+vector<double> number_multiply_vector(vector<double> array, double number){
     vector<double> ans;
-    ans.resize(df.size());
-    for(int i = 0; i < df.size(); i++) {
-        ans[i] = df[i][col];
+    const short unsigned len = array.size();
+    unsigned i;
+    ans.resize(len);
+    for (i=0;i<len;i++){
+        ans[i] = array[i] * number;
     }
+    return ans;
+}
+//compute the tensor product of certain vector, return a matrix
+vector<vector<double>> tensor_product_of_vector(vector<double> array, double mutipler){
+    vector<vector<double>> ans;
+    const short unsigned len = array.size();
+    unsigned i,j;
+    ans.resize(len);
+
+    for (i=0;i<len;i++){
+        ans[i].resize(len);
+        for (j=0;j<len;j++){
+            ans[i][j] = array[i]*array[j]*mutipler;
+        }
+    }
+    return ans;
+}
+//Compute the subtract of matrix
+vector<vector<double>> subtract_of_matrix(vector<vector<double>> matrix1, vector<vector<double>> matrix2){
+    vector<vector<double>> ans;
+    //Get the row of matrix
+    const unsigned short row = matrix1.size();
+    //Get the column of matrix
+    const unsigned short col = matrix1[0].size();
+    ans.resize(row);
+    for (unsigned i=0;i<row;i++){
+        ans[i].resize(col);
+        for (unsigned j=0;j<col;j++){
+            ans[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+
     return ans;
 }
 
 
-int main() {
-    freopen("../nnew_500.txt", "r", stdin);
-    double val;
-    vector< vector<double> > data;
-    vector< vector<double> > data_test;
-    vector<double> row;
-    vector<double> S;
-    vector<double> S_test;
 
-    int days, n;
-    scanf("%d%d", &days, &n);
-    for(int i = 0; i < days; i++) {
-        if(i > 232) {
-            row.clear();
-            for(int j = 0; j < n; j++) {
-                scanf("%lf", &val);
-                row.push_back(val);
+//Implement the power mathod to compute first k eigenvalues, return them as a vector
+vector<double> power(int k,vector<vector<double>> matrix){
+    vector<double> res;
+    const int max_iters = 1000;
+    double oldnorm2, norm2;
+    const double tolerance = 1e-10;
+    double lamda;
+    vector<double> u;
+    vector<double> v,v1;
+    // Generate the random vector u
+    srand((unsigned)time(0));
+    for (unsigned i=0;i<matrix[0].size();i++){
+        u.push_back((rand() % (10) + 1));
+    }
+    // Compute its k largest eigenvalue by recursion
+    for (unsigned i=0;i<k;i++){
+        lamda=0;
+        // Initiailize norm
+        norm2 = getnorm(u);
+        // normalize u
+        v = number_multiply_vector(u, 1/norm2);
+        for (unsigned j=1;j<=max_iters;j++){
+            // Store the previous norm
+            oldnorm2 = norm2;
+            // Compute new vector
+            v1 = matrix_multiply_array(matrix, v);
+            //Renew the norm
+            norm2 = getnorm(v1);
+            // normalize v
+            v = number_multiply_vector(v1, 1/norm2);
+            //Renew "eigenvalue"
+            lamda = norm2;
+            //Stopping rule
+            if (fabs(norm2-oldnorm2) < tolerance){
+                break;
             }
-            data_test.push_back(row);
-            scanf("%lf", &val);
-            S_test.push_back(val);
+
         }
-        if (i <= 232) {
-            row.clear();
-            for (int j = 0; j < n; j++) {
-                scanf("%lf", &val);
-                row.push_back(val);
-            }
-            data.push_back(row);
-            scanf("%lf", &val);
-            S.push_back(val);
-        }
-    }  // finished reading in here
-
-    //S = normalize(S);
-//    for(int i = 0; i < S.size(); i++) {
-//        cout << S[i] << endl;
-//    }
-//    vector< vector<double> > data1 = data;  // no need to resize then
-//    for(int i = 0; i < data[0].size(); i++) {
-//        vector<double> temp = normalize(get_column(data, i));
-//        for(int j = 0; j < data.size(); j ++) {
-//            data1[j][i] = temp[j];
-//        }
-//    }
-    //data = data1;
-    // cout << data[0][0] << " " << data[0][1] << " " << data[1][0] << " " << data[1][1] << endl;
-
-    // data 466 * 483
-//    // S 466
-//    for(int i = 0; i < days; i++) {
-//        for(int j = 0; j < n; j++)
-//            printf("%.3lf%c", data[i][j], " \n"[j + 1 == n]);
-//        printf("\n");
-//    }
-
-    // random initialize
-    vector<double> zero1;
-    vector<double> zero2;
-    vector<double> zero3;
-    vector< vector<double> > w1;
-
-    for (int i = 0; i < data[0].size();i++){
-        zero1.push_back(0);
-    }
-    for (int i = 0; i < 50 ;i++){
-        zero2.push_back(0);
-    }
-    for (int i = 0; i < 5 ;i++){
-        zero3.push_back(0);
-    }
-    for (int i = 0; i < data.size();i++){
-        X.push_back(zero1);
-        Y.push_back(zero2);
-        Z.push_back(zero3);
-        F.push_back(0);
-    }
-//    X.resize(data.size());
-//    Y.resize(data.size());
-//    Z.resize(data.size());
-//    F.resize(data.size());
-
-//    for(int i = 0; i < X.size(); i++) {
-//        X[i].resize(data[i].size());
-//        Y[i].resize(50);
-//        Z[i].resize(5);
-//    }
-
-
-
-    for(int i = 0; i < 483; i++){
-        w1.push_back(zero2);
-    }
-    for(int i = 0; i < 483; i++) {
-        for(int j = 0; j < 50; j++){
-            w1[i][j] = GaussRand(0,1);  // random
-        }
-    }
-    vector< vector<double> > w2;
-
-    for(int i = 0; i < 50; i++){
-        w2.push_back(zero3);
-    }
-    for(int i = 0; i < 50; i++) {
-        for(int j = 0; j < 5; j++){
-            w2[i][j] = GaussRand(0,1);  // same, random
-        }
-    }
-    vector<double> w3;
-    for(int i = 0; i < 5; i++){
-        w3.push_back(GaussRand(0,1));
-    }
-
-    // initialize weights's, and a trivial loss value
-    // double loss = 0;
-    // main recursion of GD
-    double loss = 0;
-    double new_loss = 0;
-    double trial_loss = 0;
-    double trial_loss_2 = 0;
-    double loss_test = 0;
-    vector<vector<double> > charlie;
-    for (int i = 0; i < 483; i++){
-        charlie.push_back(zero2);
-    }
-
-    vector<vector<double> > bravo;
-    for (int i = 0; i < 50; i++){
-        bravo.push_back(zero3);
+        // Store the eigenvalue in result array one by one
+        res.push_back(lamda);
+        // renew the matrix in order to compute the next eigenvalue
+        matrix = subtract_of_matrix(matrix, tensor_product_of_vector(v, lamda));
     }
 
 
-    vector<double> alpha;
-    alpha = zero3;
+    return res;
+}
 
-    long double lambda;
-    vector<vector<double> > v1;
-    vector<vector<double> > v2;
-    vector<double> v3;
-    for(int _ = 0; _ < 25; _++) {
-        calc_output(w1, w2, w3, data);
-        new_loss = get_loss(S);
+int main(void)
+{
+    //vector<vector<int>> matrix = { { 2, 1 }, { 4, 3 } };
+    //vector<double> arrB = { { 1, 2 }, { 1, 0 } };
+    vector<vector<double>> matrix = { { 0.7676, 0.1907, -0.1723,-0.0707 }, { 0.1907, 0.6843, 0.2717 ,0.1093} ,{-0.1723,0.2717,0.4841,-0.1342},{-0.0707,0.1093,-0.1342,0.8610}};
+    vector<double> res = power(2, matrix);
+    for (unsigned i=0;i<res.size();i++){
 
-//        for(int t = 0; t < data.size(); t++) {
-//            cout << S[t] << endl;
-//        }
-        cout << new_loss << endl;
-      //  if(abs(loss - new_loss) / loss < 0.0001) {
-        //    break;
-   // }
-        //loss = new_loss;
-        // let's start calculating the gradient
+        cout<<res[i]<<" ";
 
-        for (int i = 0; i < 483; i++) {
-            for (int j = 0; j < 50; j++) {
-                charlie[i][j] = 0;  // random
-            }
-        }
-
-        for (int i = 0; i < 50; i++) {
-            for (int j = 0; j < 5; j++) {
-                bravo[i][j] = 0;  // same, random
-            }
-        }
-
-        for (int i = 0; i < 5; i++) {
-            alpha[i] = 0;
-        }
-
-        // iterate over the dates to calc the average gradient
-        for (int t = 0; t < data.size(); t++) {
-            // last layer gradient
-            for (int i = 0; i < 5; i++) {
-                alpha[i] += 2 * (F[t] - S[t]) * Z[t][i] / data.size();  // just to avoid infinite divisions in the end
-            }
-            // second layer gradient
-            for (int i = 0; i < 50; i++) {
-                for (int j = 0; j < 5; j++) {
-                    bravo[i][j] += 2 * (F[t] - S[t]) * w3[j] * f_prime(Z[t][j]) * Y[t][i] / data.size();
-                }
-            }
-            // first layer gradient
-            for (int k = 0; k < 5; k++) {
-                // printf("%d\n", k);
-                for (int i = 0; i < 483; i++) {
-                    for (int j = 0; j < 50; j++) {
-                        charlie[i][j] += 2 * (F[t] - S[t]) *
-                                         w3[k] * f_prime(Z[t][k]) * w2[k][j] * f_prime(Y[t][j]) * X[t][i] / data.size();
-                    }
-                }
-            }
-
-        }
-
-        // w1 - charlie, w2 - bravo, w3 - alpha
-        lambda = 1e-13;
-        loss = new_loss;
-
-
-        v1 = w1;
-        v2 = w2;
-        v3 = w3;
-
-        for (int i = 0; i < 483; i++) {
-            for (int j = 0; j < 50; j++) {
-                v1[i][j] -= lambda * charlie[i][j];
-            }
-        }
-
-        for (int i = 0; i < 50; i++) {
-            for (int j = 0; j < 5; j++) {
-                // cout << w2[i][j] << endl;
-                v2[i][j] -= lambda * bravo[i][j];
-            }
-        }
-
-        for (int i = 0; i < 5; i++) {
-            v3[i] -= lambda * alpha[i];
-            // cout << w3[i] << endl;
-        }
-        calc_output(v1, v2, v3, data);
-        trial_loss = get_loss(S);
-            while (trial_loss > loss) {
-                lambda /= 2;
-//                if (lambda < 1e-20){
-//                    lambda = 1e-17;
-//                    break;
-//                }
-
-                v1 = w1;
-                v2 = w2;
-                v3 = w3;
-                for (int i = 0; i < 483; i++) {
-                    for (int j = 0; j < 50; j++) {
-                        v1[i][j] -= lambda * charlie[i][j];
-                    }
-                }
-
-                for (int i = 0; i < 50; i++) {
-                    for (int j = 0; j < 5; j++) {
-                        // cout << w2[i][j] << endl;
-                        v2[i][j] -= lambda * bravo[i][j];
-                    }
-                }
-
-                for (int i = 0; i < 5; i++) {
-                    v3[i] -= lambda * alpha[i];
-                    // cout << w3[i] << endl;
-                }
-                calc_output(v1, v2, v3, data);
-                trial_loss = get_loss(S);
-                cout << trial_loss<< " "<< loss << endl;
-                cout << "lambda" << ' '<< lambda << endl;
-            }
-
-
-
-            trial_loss_2 = trial_loss;
-            while (trial_loss_2 <= trial_loss) {
-                lambda /= 1.3;
-                v1 = w1;
-                v2 = w2;
-                v3 = w3;
-                for (int i = 0; i < 483; i++) {
-                    for (int j = 0; j < 50; j++) {
-                        v1[i][j] -= lambda * charlie[i][j];
-                    }
-                }
-
-                for (int i = 0; i < 50; i++) {
-                    for (int j = 0; j < 5; j++) {
-                        // cout << w2[i][j] << endl;
-                        v2[i][j] -= lambda * bravo[i][j];
-                    }
-                }
-
-                for (int i = 0; i < 5; i++) {
-                    v3[i] -= lambda * alpha[i];
-                    // cout << w3[i] << endl;
-                }
-                calc_output(v1, v2, v3, data);
-                trial_loss = trial_loss_2;
-                trial_loss_2 = get_loss(S);
-
-                cout << "lamda2" << ' ' << lambda << endl;
-                cout << "trial_loss_2"<< trial_loss_2 << endl;
-
-            }
-            lambda *= 1.3;
-            cout << " Lambda: "<< lambda << endl;
-        for(int i = 0; i < 483; i++) {
-            for(int j = 0; j < 50; j++) {
-                w1[i][j] -= lambda * charlie[i][j];
-            }
-        }
-
-        for(int i = 0; i < 50; i++) {
-            for(int j = 0; j < 5; j++) {
-                // cout << w2[i][j] << endl;
-                w2[i][j] -= lambda * bravo[i][j];
-            }
-        }
-
-        for(int i = 0; i < 5; i++) {
-            w3[i] -= lambda * alpha[i];
-            // cout << w3[i] << endl;
-        }
     }
-
-    loss_test = get_loss(S_test);
-    cout << "lost_test"<<" "<<loss_test<< endl;
     return 0;
 }
 
-//    vector<double> v= {1, 2};
-//    vector< vector<double>> w = {{1, 2}, {2, 3}};
-//    vector<double> ans = matrix_multiply_array(w, v);
-//    for (unsigned i = 0; i < ans.size(); i++) {
-//        cout << ans[i] << " ";
-//    }
